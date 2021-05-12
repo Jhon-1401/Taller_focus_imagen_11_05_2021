5d5c559e9d2c0a09ca970ffac9af984085660a9a
Jhon-1401 <jhonguevara1401@gmail.com>
2021-05-11 21:13:59 -0500

    11-05-2021

---
+++ dist/dist/main.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.onload = function () {
+  var $ = document;
+  $.addEventListener("click", function (event) {
+    if (event.target.children.length == 0) {
+      if (event.path[2].firstElementChild.getAttribute("style")) event.path[2].firstElementChild.setAttribute('style', event.target.getAttribute("coords"));else event.path[2].firstElementChild.setAttribute('style', event.target.getAttribute("coords"));
+    } else event.path[1].firstElementChild.removeAttribute('style');
+  }, false);
+};
